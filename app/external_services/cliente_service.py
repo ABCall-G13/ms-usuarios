1e4200d38becbcd0abceffadcf32d36a4202ea93
Sebaschb <schaconb@uniandes.edu.co>
2024-11-02 23:16:06 -0500

    Add URL_SERVICE_CLIENT configuration and update cliente_service to use it

--- app/external_services/cliente_service.py
+++ app/external_services/cliente_service.py
@@ -1,13 +1,16 @@
 import httpx
 from fastapi import HTTPException
+from app import config
 
 
 async def verificar_cliente_existente(nit_cliente: str):
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"https://ms-clientes-345518488840.us-central1.run.app/clientes/{nit_cliente}")
+        response = await client.get(
+            f"{config.URL_SERVICE_CLIENT}/clientes/{nit_cliente}")
         if response.status_code == 404:
             raise HTTPException(
                 status_code=404, detail="Cliente no encontrado")
         elif response.status_code != 200:
             raise HTTPException(status_code=response.status_code,
                                 detail="Error al verificar el cliente")
+        
