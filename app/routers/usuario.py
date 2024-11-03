5b8d1ec543e2d7d6676e0a174dffc822414c1786
Sebaschb <schaconb@uniandes.edu.co>
2024-11-02 23:53:52 -0500

    Add utility functions for Excel file processing and update user synchronization logic

--- app/routers/usuario.py
+++ app/routers/usuario.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter, Depends, File, HTTPException, UploadFile
 from sqlalchemy.orm import Session
 from app.external_services.cliente_service import verificar_cliente_existente
 from app.schemas.usuario import UserResponse
-from app.crud.usuario import get_user_by_document_and_client, create_or_update_user_client_data
+from app.crud.usuario import get_user_by_document_and_client
 from app.dependencies import get_db
 import pandas as pd
 import httpx
+from app.utils.utils import verificar_tipo_archivo, leer_archivo_excel, verificar_columnas_requeridas, procesar_filas_y_actualizar_db
 
 router = APIRouter()
 
@@ -29,33 +30,11 @@ async def sync_users(
     file: UploadFile = File(...),
     db: Session = Depends(get_db)
 ):
-
     await verificar_cliente_existente(nit_cliente)
+    verificar_tipo_archivo(file)
 
-    if file.content_type != 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-        raise HTTPException(
-            status_code=400, detail="El archivo debe ser de tipo Excel (.xlsx)")
+    df = leer_archivo_excel(file.file)
+    verificar_columnas_requeridas(df)
+    procesar_filas_y_actualizar_db(df, nit_cliente, db)
 
-    try:
-
-        df = pd.read_excel(file.file)
-
-        required_columns = {"doc_type", "doc_number",
-                            "nombre", "email", "telefono"}
-        if not required_columns.issubset(df.columns):
-            raise HTTPException(
-                status_code=400,
-                detail=f"El archivo debe contener las columnas: {', '.join(required_columns)}"
-            )
-
-        for _, row in df.iterrows():
-            row_data = row.to_dict()
-
-            row_data['nit_cliente'] = nit_cliente
-            create_or_update_user_client_data(db, row_data)
-
-        return {"detail": "Datos sincronizados exitosamente"}
-
-    except Exception as e:
-        raise HTTPException(
-            status_code=500, detail=f"Error al procesar el archivo: {str(e)}")
+    return {"detail": "Datos sincronizados exitosamente"}
